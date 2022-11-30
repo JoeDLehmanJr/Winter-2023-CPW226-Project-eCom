a6ce16e82ef1967fc7d8f8058d06b36891edc941
JoeDLehmanJr <lehman5247@students.cptc.edu>
2022-11-30 15:00:21 -0800

    forgot to attach this to the last commit.

--- CPW221-Project-eCom/Models/Invoices.cs
+++ CPW221-Project-eCom/Models/Invoices.cs
@@ -1,5 +1,7 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace CPW221_Project_eCom.Models
 {
     /// <summary>
@@ -20,6 +22,8 @@ namespace CPW221_Project_eCom.Models
         /// convert to string?
         ///  NOT NULL
         /// </summary>
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
 
         /// <summary>
@@ -34,18 +38,36 @@ namespace CPW221_Project_eCom.Models
         /// needs construction of way to handle datetime data
         /// convert to string?
         /// </summary>
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
 
         /// <summary>
         /// Id of the customer assigned to product. Foreign Key. Connects to Customer
         ///  NOT NULL
         /// </summary>
-        public int CustomerId { get; set; }
+        public int invoicesCustomerId { get; set; }
 
         //PLACEHOLDER
         /// <summary>
         /// Determines if the order was made online or by other means and returns boolean value
         /// </summary>
         public bool isOnlineOrder;
+
+        /// <summary>
+        /// makes the customer class into a object
+        /// </summary>
+        public ICollection<Customer> _Customer { get; set; }
+
+        /// <summary>
+        /// pointer to link the invoices items table to this one
+        /// </summary>
+        public int OrderNumberId { get; set; }
+
+        /// <summary>
+        /// collection of invoice items
+        /// </summary>
+        public InvoicedItems _InvoicedItems { get; set; }
+
     }
 }
