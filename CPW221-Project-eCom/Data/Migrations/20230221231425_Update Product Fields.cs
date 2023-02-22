6197ecdcf7cb091c86255d7765898f13a1c0d873
JoeDLehmanJr <lehman5247@students.cptc.edu>
2023-02-21 16:21:16 -0800

    Changed Product Description to a textarea. Added Product Catalog to the menu. Changed navbar-brand & TItle to Joe's Software Store. Did Add-Migration & Update-Database.

---
+++ CPW221-Project-eCom/Data/Migrations/20230221231425_Update Product Fields.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CPW221_Project_eCom.Data.Migrations
+{
+    public partial class UpdateProductFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Product",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Product");
+        }
+    }
+}
