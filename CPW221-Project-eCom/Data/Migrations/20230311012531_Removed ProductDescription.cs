cff288a389e3b106ff303e2539af0d322a7035ac
JoeDLehmanJr <lehman5247@students.cptc.edu>
2023-03-10 18:29:03 -0800

    Got rid of productDescription in the ProductCategory table and ran Update-Database to make the changes in the database.

---
+++ CPW221-Project-eCom/Data/Migrations/20230311012531_Removed ProductDescription.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CPW221_Project_eCom.Data.Migrations
+{
+    public partial class RemovedProductDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCategoryDescription",
+                table: "ProductCategory");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCategoryDescription",
+                table: "ProductCategory",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
